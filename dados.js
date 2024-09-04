37a5cf4c416b6439e0ad145f5a43ff9a93c7c837
Gabriel <gabriel.molinari2007@gmail.com>
2024-09-04 15:58:27 -0300

    new version

---
+++ dados.js
@@ -0,0 +1,38 @@
+let dados = [
+    {
+        universo: 'Dragon Ball', // Nome do universo
+        descricao: 'Um universo onde a luta e o treinamento são essenciais para se tornar mais forte.',
+        raças: ['Saiyajin', 'Namekuseijin', 'Humano', 'Majin'],
+        artefatos: ['Esferas do Dragão', 'Bastão do Poder'],
+        tecnologias: ['Máquina do Tempo', 'Sala do Tempo'],
+        planetas: ['Terra', 'Namek', 'Vegeta'], // Planetas chave
+        poderes: ['Ki', 'Transformação', 'Fusão'], // Habilidades e poderes
+        deidades: ['Deus dos Dragões', 'Kaioshin'], // Deidades e entidades divinas
+        organizacoes: ['Patrulha Galáctica'], // Grupos e facções
+        conceitos: ['Super Saiyajin', 'Zenkai'], // Conceitos-chave
+    },
+    {
+        universo: 'Marvel',
+        descricao: 'Um universo interconectado de super-heróis e vilões, onde eventos cósmicos e conflitos interdimensionais são comuns.',
+        raças: ['Humano', 'Mutante', 'Asgardiano', 'Inumano', 'Kree', 'Skrull'],
+        artefatos: ['Mjolnir', 'Escudo do Capitão América', 'Guantelete do Infinito'],
+        tecnologias: ['Reator Ark', 'Viagem Interdimensional', 'Armas Vibranium'],
+        planetas: ['Terra', 'Asgard', 'Wakanda', 'Hala'],
+        poderes: ['Super força', 'Voo', 'Manipulação da energia', 'Regeneração'],
+        deidades: ['Odin', 'Celestiais'],
+        organizacoes: ['Vingadores', 'X-Men', 'SHIELD', 'Hydra'],
+        conceitos: ['Multiverso', 'Mutação', 'Radiação Gama'],
+    },
+    {
+        universo: 'Star Wars',
+        descricao: 'Uma galáxia distante, governada pela Força, onde a luta entre o bem e o mal molda o destino de muitos.',
+        raças: ['Humano', 'Twi-lek', 'Wookiee', 'Droid', 'Jedi', 'Sith'],
+        artefatos: ['Sabre de Luz', 'Holocron', 'Sfera Sith'],
+        tecnologias: ['Hiperdrive', 'Blasters', 'Estrela da Morte'],
+        planetas: ['Tatooine', 'Alderaan', 'Coruscant', 'Endor'],
+        poderes: ['Força', 'Sentidos aprimorados'],
+        deidades: ['Força'],
+        organizacoes: ['Império Galáctico', 'República Galáctica', 'Ordem Jedi'],
+        conceitos: ['Lado Luminoso', 'Lado Sombrio', 'Midichlorians']
+    }
+];
