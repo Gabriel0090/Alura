9ae40748aad53e3d974badffb4cb43d3fb6da773
Gabriel <gabriel.molinari2007@gmail.com>
2025-02-28 10:55:04 -0300

    Resultados de pesquisa

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 function pesquisar() {
-
     let section = document.getElementById("resultados-pesquisa");
-    console.log(section);
-    
-    for (let dado of dados) {
-        section.innerHTML += `
-            <div class="item-resultado">
-                <h2>
-                    <a href="#" target="_blank">${dado.universo}</a>
-                </h2>
-                <p class="descricao-meta">${dado.descricao}</p>
-                <a href="${dado.link}" target="_blank"> mais info</a>
-            </div>
-        `;
+    let campoPesquisa = document.getElementById("campo-pesquisa").value.toLowerCase(); // Convertendo para minúsculas
+
+    if (campoPesquisa === "") {
+        section.innerHTML = "<p>Nada foi encontrado</p>";
+        return;
     }
+
+    let resultados = "";
+
+    for (let dado of dados) {
+        const universo = dado.universo.toLowerCase();
+        const descricao = dado.descricao.toLowerCase();
+
+        if (universo.includes(campoPesquisa) || descricao.includes(campoPesquisa)) {
+            resultados += `
+                <div class="item-resultado">
+                    <h2>
+                        <a href="#" target="_blank">${dado.universo}</a>
+                    </h2>
+                    <p class="descricao-meta">${dado.descricao}</p>
+                    <a href="${dado.link}" target="_blank"> mais info</a>
+                </div>
+            `;
+        }
+    }
+
+
+    if (!resultados){
+resultados= "<p>Nada foi encontrado</p>"
+    }
+
+    section.innerHTML = resultados; // Inserindo os resultados na seção
 }
+
--- dados.js
+++ dados.js
@@ -2,37 +2,54 @@ let dados = [
     {
         universo: 'Dragon Ball', // Nome do universo
         descricao: 'O universo Dragon Ball é um vasto cenário de aventuras, artes marciais e buscas por poder, criado pelo mangaká Akira Toriyama. A história gira em torno de Son Goku, um jovem saiyan com uma força extraordinária, e sua jornada para se tornar o guerreiro mais forte do universo.',
-        raças: ['Saiyajin', 'Namekuseijin', 'Humano', 'Majin'],
-        artefatos: ['Esferas do Dragão', 'Bastão do Poder'],
-        tecnologias: ['Máquina do Tempo', 'Sala do Tempo'],
-        planetas: ['Terra', 'Namek', 'Vegeta'], // Planetas chave
-        poderes: ['Ki', 'Transformação', 'Fusão'], // Habilidades e poderes
-        deidades: ['Deus dos Dragões', 'Kaioshin'], // Deidades e entidades divinas
-        organizacoes: ['Patrulha Galáctica'], // Grupos e facções
-        conceitos: ['Super Saiyajin', 'Zenkai'], // Conceitos-chave
     },
     {
         universo: 'Marvel',
         descricao: 'O Universo Marvel é um vasto universo compartilhado, criado pela Marvel Comics, onde se passam as aventuras de inúmeros super-heróis e vilões. É como um grande playground onde personagens como Homem de Ferro, Thor, Capitão América, Hulk, Homem-Aranha e muitos outros interagem em histórias épicas e emocionantes.',
-        raças: ['Humano', 'Mutante', 'Asgardiano', 'Inumano', 'Kree', 'Skrull'],
-        artefatos: ['Mjolnir', 'Escudo do Capitão América', 'Guantelete do Infinito'],
-        tecnologias: ['Reator Ark', 'Viagem Interdimensional', 'Armas Vibranium'],
-        planetas: ['Terra', 'Asgard', 'Wakanda', 'Hala'],
-        poderes: ['Super força', 'Voo', 'Manipulação da energia', 'Regeneração'],
-        deidades: ['Odin', 'Celestiais'],
-        organizacoes: ['Vingadores', 'X-Men', 'SHIELD', 'Hydra'],
-        conceitos: ['Multiverso', 'Mutação', 'Radiação Gama'],
     },
     {
         universo: 'Star Wars',
         descricao: 'Star Wars é uma saga épica de ficção científica que se passa em uma galáxia distante, repleta de planetas, espécies alienígenas e tecnologias avançadas. A história se concentra na luta entre o bem e o mal, representados pela Aliança Rebelde e o Império Galáctico, respectivamente.',
-        raças: ['Humano', 'Twi-lek', 'Wookiee', 'Droid', 'Jedi', 'Sith'],
-        artefatos: ['Sabre de Luz', 'Holocron', 'Sfera Sith'],
-        tecnologias: ['Hiperdrive', 'Blasters', 'Estrela da Morte'],
-        planetas: ['Tatooine', 'Alderaan', 'Coruscant', 'Endor'],
-        poderes: ['Força', 'Sentidos aprimorados'],
-        deidades: ['Força'],
-        organizacoes: ['Império Galáctico', 'República Galáctica', 'Ordem Jedi'],
-        conceitos: ['Lado Luminoso', 'Lado Sombrio', 'Midichlorians']
+    },
+    {
+        universo: 'Harry Potter',
+        descricao: 'O mundo mágico de Harry Potter, criado por J.K. Rowling, é um lugar onde bruxos e bruxas vivem em segredo entre os trouxas. A história gira em torno do jovem bruxo Harry Potter e seus amigos, que frequentam a Escola de Magia e Bruxaria de Hogwarts.'
+    },
+    {
+        universo: 'O Senhor dos Anéis',
+        descricao: 'Criado por J.R.R. Tolkien, a Terra Média é um mundo de fantasia repleto de criaturas míticas, como hobbits, elfos, anões e orcs. A história acompanha Frodo Bolseiro em sua jornada para destruir o Um Anel e impedir que o Senhor das Trevas, Sauron, conquiste a Terra Média.'
+    },
+    {
+        universo: 'Star Trek',
+        descricao: 'Star Trek é uma franquia de ficção científica que explora temas como exploração espacial, diversidade e o futuro da humanidade. A série acompanha as aventuras da tripulação da Enterprise, uma nave espacial que explora a galáxia.'
+    },
+    {
+        universo: 'Dune',
+        descricao: 'Baseado na obra de Frank Herbert, Dune é um universo de ficção científica ambientado em um futuro distante, onde a humanidade luta pelo controle de um planeta árido e valioso, chamado Arrakis. A história gira em torno de Paul Atreides, um jovem que se torna o líder dos Fremen e um messias para os habitantes de Arrakis.'
+    },
+    {
+        universo: 'Cyberpunk 2077',
+        descricao: 'Cyberpunk 2077 é um mundo futurista distópico, onde a tecnologia cibernética está integrada à vida cotidiana e as megacorporações controlam a sociedade. A história se passa na cidade de Night City, um lugar perigoso e cheio de oportunidades.'
+    },
+    {
+        universo: 'Mad Max',
+        descricao: 'O mundo de Mad Max é um futuro pós-apocalíptico, onde a sociedade entrou em colapso e a violência é a lei. A história acompanha Max Rockatansky, um guerreiro solitário que luta para sobreviver em um mundo hostil.'
+    },
+    {
+        universo: 'Avatar',
+        descricao: 'Pandora é um planeta exuberante e alienígena, habitado por seres humanoides chamados Navi.A história de Avatar acompanha Jake Sully, um ex- marine que se conecta mentalmente a um avatar Navi e se envolve em uma luta pela sobrevivência do povo de Pandora.'
+    },
+    {
+        universo: 'Rick and Morty',
+        descricao: 'Rick and Morty é uma animação adulta que acompanha as aventuras de Rick Sanchez, um cientista brilhante e alcoólatra, e seu neto Morty Smith, um adolescente inseguro. Juntos, eles viajam por diferentes dimensões e universos paralelos.'
+    },
+    {
+        universo: 'The Witcher',
+        descricao: 'O mundo de The Witcher é um universo de fantasia medieval, onde monstros e magia coexistem com os humanos. A história acompanha Geralt de Rivia, um bruxo que caça monstros e se envolve em intrigas políticas.'
+    },
+    {
+        universo: 'Attack on Titan',
+        descricao: 'Em um mundo pós-apocalíptico, a humanidade vive dentro de cidades muradas para se proteger dos Titãs, criaturas gigantescas que devoram humanos. A história acompanha Eren Yeager, um jovem que jura exterminar todos os Titãs após a morte de sua mãe.'
     }
+
 ];
